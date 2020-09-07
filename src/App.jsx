89ad9bde1c41686c84d8e1ec565fbabbf6a88254
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-09-07 11:17:41 +0500

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -27,23 +27,26 @@ class App extends React.Component {
   render() {
     const quote = this.state.quote;
     return (
-      <div id="quote-box">
+      <div className="row">
+        <div id="quote-box" className="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2 col-10 offset-1 col-xs-10 offset-xs-1">
+          <blockquote class="blockquote text-center">
+            <p id="text" class="mb-0">{quote && quote.text}</p>
+            <footer id="author" class="blockquote-footer">{quote && quote.author}</footer>
+          </blockquote>
 
-        <p id="text">{quote && quote.text}</p>
-
-        <span id="author">{quote && quote.author}</span>
-
-        <div className="row"> 
-          <div className="col-md-6">
-            <a id="tweet-quote" href="twitter.com/intent/tweet">
-            tweet
-             </a>
-          </div>
-          <div className="col-md-6">
-            <button className="btn btn-primary" id="new-quote" onClick={this.randomQuote}>New</button>
+          <div className="row">
+            <div className="col-3 offset-2">
+              <a class="btn btn-lg btn-primary" id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </div>
+            <div className="col-3 offset-2">
+              <button className="btn btn-lg btn-primary" id="new-quote" onClick={this.randomQuote}>New</button>
+            </div>
           </div>
         </div>
       </div>
+
     );
   }
 }
